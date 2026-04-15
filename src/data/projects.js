0473a2f3a11047349b665417c1473953ffa40d0b
iam3zero <vergalyoung@gmail.com>
2026-04-15 17:47:32 +0900

    vercel 배포오류 이미지경로수정

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,6 +7,9 @@ import passport from "../assets/img/passport_fullpage.png";
 import bluebottle from "../assets/img/bluebottle_fullpage.png";
 import shootingGame from "../assets/img/shootingGame_fullpage.png";
 
+import eclat_projectDetail from "../assets/img/eclat_projectDetail.png";
+import monitorImg from "../assets/img/projects_monitor.png";
+
 import Photoshop from "../assets/icons/project_Photoshop.png";
 import Figma from "../assets/icons/project_Figma.png";
 import React from "../assets/icons/project_React.png";
@@ -15,6 +18,8 @@ import SCSS from "../assets/icons/Project_SCSS.png";
 import HTML from "../assets/icons/project_HTML.png";
 import CSS from "../assets/icons/project_CSS.png";
 import Javascript from "../assets/icons/project_Javascript.png";
+
+
 
 const projects = [
   {
@@ -26,7 +31,7 @@ const projects = [
     duration: "2주",
     members: "4명 (TeamProject)",
     tools: "Adobe Photoshop | Figma | SCSS | React | Vercel",
-    designImage: "/images/project1-design.png",
+    designImage: eclat_projectDetail,
     description: "보이지 않는 빛을 향으로 표현하는 가상의 비건 감성 향수 브랜드 ‘ÉCLAT’를 기획 및 디자인하여 웹사이트로 구현하였습니다. 보이지 않는 빛을 향으로 표현하는 가상의 비건 감성 향수 브랜드 ‘ÉCLAT’를 기획 및 디자인하여 웹사이트로 구현하였습니다.",
 
     concept: {
@@ -44,7 +49,7 @@ const projects = [
 
     result: "ÉCLAT는 보이지 않는 내면의 빛을 향으로 풀어내는 브랜드입니다. 눈에 보이는 것 너머에는 사람마다 고유한 빛이 존재합니다. 향은 단순한 냄새가 아니라, 기억을 깨우고 감정을 움직이며 한 사람의 분위기를 완성하는 또 하나의 언어입니다. 각각의 향은 하나의 빛이 되어, 당신의 순간에 스며들고 당신을 더욱 빛나게 합니다..",
 
-    monitor: "../assets/img/projects_monitor.png",
+    monitor: monitorImg,
 
     website: "https://eclat-t.vercel.app/",
     github: "https://github.com/seongeun040624/eclatT",
@@ -66,7 +71,7 @@ const projects = [
     duration: "3주",
     members: "4명 (TeamProject)",
     tools: "Figma | SCSS | React | Vercel",
-    designImage: "/images/project1-design.png",
+    designImage: eclat_projectDetail,
     description: "OpenWeather API 및 위치 데이터 활용하여 접속 즉시 '날씨 + 위치'를 반영해 최적화된 플레이리스트를 추천해주는 웹사이트를 REACT로 작업한 첫번째 팀 프로젝트입니다.",
     concept: {
       typography: "Pretendard",
@@ -83,7 +88,7 @@ const projects = [
 
     result: "ÉCLAT는 보이지 않는 내면의 빛을 향으로 풀어내는 브랜드입니다. 눈에 보이는 것 너머에는 사람마다 고유한 빛이 존재합니다. 향은 단순한 냄새가 아니라, 기억을 깨우고 감정을 움직이며 한 사람의 분위기를 완성하는 또 하나의 언어입니다. 각각의 향은 하나의 빛이 되어, 당신의 순간에 스며들고 당신을 더욱 빛나게 합니다..",
 
-    monitor: "../assets/img/projects_monitor.png",
+    monitor: monitorImg,
 
     website: "https://mood-scape-umber.vercel.app/",
     github: "https://github.com/ksyeon068/MoodScape",
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,6 +7,8 @@ import passport from "../assets/img/passport_fullpage.png";
 import bluebottle from "../assets/img/bluebottle_fullpage.png";
 import shootingGame from "../assets/img/shootingGame_fullpage.png";
 
+import monitorImg from "../assets/img/projects_monitor.png";
+
 import Photoshop from "../assets/icons/project_Photoshop.png";
 import Figma from "../assets/icons/project_Figma.png";
 import React from "../assets/icons/project_React.png";
@@ -31,8 +33,8 @@ export const projectsData = [
     description:
       "보이지 않는 빛을 향으로 표현하는 가상의 국내 니치향수 브랜드 ‘ÉCLAT’를 기획 및 디자인하여 웹사이트로 구현하였습니다. 제품 라인 및 패키지 기획부터 주요 타켓층을 설정하여 감성 중심의 웹사이트로 구현 방향을 정하고 웹 프론트를 REACT로 작업하였습니다.",
 
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
+    
 
     website: "https://eclat-t.vercel.app/",
     github: "https://github.com/seongeun040624/eclatT",
@@ -57,8 +59,7 @@ export const projectsData = [
     tools: "Figma | SCSS | React | Vercel",
     description: "OpenWeather API 및 위치 데이터 활용하여 접속 즉시 '날씨 + 위치'를 반영해 최적화된 플레이리스트를 추천해주는 웹사이트를 REACT로 작업한 첫번째 팀 프로젝트입니다.",
 
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
 
     website: "https://mood-scape-umber.vercel.app/",
     github: "https://github.com/ksyeon068/MoodScape",
@@ -82,8 +83,7 @@ export const projectsData = [
     tools: "HTML | CSS | JAVASCRIPT | API",
     description: "기존 디자인호텔스 웹사이트를 참고하여 메인페이지를 반응형 페이지로 리뉴얼하여 작업했습니다. 미디어쿼리를 사용하여 모든 기기에서 보기 편하도록 작업하였습니다.",
 
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
 
     website: "https://iam3zero.github.io/hote/",
     github: "https://github.com/iam3zero/hote",
@@ -105,8 +105,9 @@ export const projectsData = [
     members: "1명",
     tools: "HTML | CSS | JAVASCRIPT",
     description: "웹표준과 웹 접근성을 중시한 외교부 클론코딩 프로젝트입니다. HTML과 CSS를 사용하여 화면을 구성했으며, 반응형 및 Swiper 라이브러리를 사용하여 슬라이드를 구현했습니다.",
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+
+    monitor: monitorImg,
+
     website: "https://iam3zero.github.io/passport/",
     github: "https://github.com/iam3zero/passport",
     toolsIcons: [
@@ -126,8 +127,7 @@ export const projectsData = [
     members: "1명",
     tools: "HTML | CSS | JAVASCRIPT",
     description: "메인페이지 클론코딩과 더불어 반응형 페이지로 리뉴얼하여 제작했습니다. 미디어쿼리를 활용하여 모바일 환경에서 이용할 수 있는 모바일 메뉴바를 구현하고, swiper 라이브러리를 활용하여 슬라이드를 구현했습니다.",
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
     website: "https://iam3zero.github.io/Bluebottle/",
     github: "https://github.com/iam3zero/Bluebottle",
     toolsIcons: [
@@ -147,7 +147,7 @@ export const projectsData = [
     members: "1명",
     tools: "HTML CANVERS | JAVASCRIPT | CSS",
     description: "HTML5 <canvas> 태그와 JavaScript를 활용하여 브라우저에서 실행되는 2D 슈팅게임을 제작하였습니다.",
-    monitor: "/images/monitor.png",
+    monitor: monitorImg,
     screen: "/images/project1.png",
     website: "https://iam3zero.github.io/shooting-game/",
     github: "https://github.com/iam3zero/shooting-game",
@@ -167,8 +167,8 @@ export const projectsData = [
     members: "1명",
     tools: "React",
     description: "현재 준비중인 프로젝트 입니다. 기대해주세요!",
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
+
     website: "#",
     github: "#",
     toolsIcons: []
@@ -183,8 +183,8 @@ export const projectsData = [
     members: "1명",
     tools: "React",
     description: "현재 준비중인 프로젝트 입니다. 기대해주세요!",
-    monitor: "/images/monitor.png",
-    screen: "/images/project1.png",
+    monitor: monitorImg,
+
     website: "#",
     github: "#",
     toolsIcons: []
